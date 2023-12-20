119887055a334b9d9cbe6c25f640cba0ef1c8d7d
danimv <danimv87@gmail.com>
2023-12-20 02:42:20 +0100

    social contrib

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assest/logo.jpg";
+import logo from "../assest/logo.png";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { BsCartFill } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
@@ -17,59 +17,59 @@ const Header = () => {
   };
   const handleLogout = () => {
     dispatch(logoutRedux());
-    toast("Logout successfully");
+    toast("Sessió tancada");
   };
 
   const cartItemNumber = useSelector((state) => state.product.cartItem);
   return (
-    <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50 bg-white">
+    <header className="fixed shadow-md w-full h-14 px-2 md:px-4 z-50 bg-white">
       {/* desktop */}
 
       <div className="flex items-center h-full justify-between">
         <Link to={""}>
           <div className="flex max-w-4xl mx-auto ">
             <img src={logo} className="rounded-full h-20 " />
-            <h2 className=" font-bold text-2xl dark:text-red-700  p-4  md: hidden md:flex  ">
-              Aana Zoor
+            <h2 className=" font-bold text-2xl text-green-700 dark:text-red-700  p-4  md: hidden md:flex  ">
+             Global Regeneration
             </h2>
           </div>
         </Link>
 
         <div className="flex items-center gap-4 md:gap-7">
-          <nav className="gap-4 md:gap-6 text-base md:text-lg hidden md:flex">
-            <Link to={""}>Home</Link>
-            <Link to={"menu/646b5548acd0a88a674b9429"}>Menu</Link>
-            <Link to={"about"}>About</Link>
-            <Link to={"contact"}>Contact</Link>
+          <nav className="gap-4 md:gap-6 text-green-700 text-base md:text-lg hidden md:flex">
+            <Link to={""}>Inici</Link>
+            {/* <Link to={"menu/646b5548acd0a88a674b9429"}>Menu</Link> */}
+            <Link to={"about"}>Sobre</Link>
+            <Link to={"contact"}>Contactar</Link>
           </nav>
-          <div className="text-2xl text-slate-600 relative">
+          {/* <div className="text-2xl text-slate-600 relative">
             <Link to={"cart"}>
               <BsCartFill />
               <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center ">
                 {cartItemNumber.length}
               </div>
             </Link>
-          </div>
+          </div> */}
           <div className=" text-slate-600" onClick={handleShowMenu}>
             <div className="text-3xl cursor-pointer w-8 h-8 rounded-full overflow-hidden drop-shadow-md">
               {userData.image ? (
                 <img src={userData.image} className="h-full w-full" />
               ) : (
-                <HiOutlineUserCircle />
+                <HiOutlineUserCircle className="font-bold text-green-700" />
               )}
             </div>
             {showMenu && (
               <div className="absolute right-2 bg-white py-2  shadow drop-shadow-md flex flex-col min-w-[120px] text-center">
-                {userData.email === process.env.REACT_APP_ADMIN_EMAIL && (
+                {/* {userData.email === process.env.REACT_APP_ADMIN_EMAIL && (
                   <Link
                     to={"newproduct"}
                     className="whitespace-nowrap cursor-pointer px-2"
                   >
                     New product
                   </Link>
-                )}
+                )} */}
 
-                {userData.image ? (
+                {userData.email === process.env.REACT_APP_ADMIN_EMAIL ? (
                   <p
                     className="cursor-pointer text-white px-2 bg-red-500"
                     onClick={handleLogout}
@@ -86,19 +86,19 @@ const Header = () => {
                 )}
                 <nav className="text-base md:text-lg flex flex-col md:hidden">
                   <Link to={""} className="px-2 py-1">
-                    Home
+                    Inici
                   </Link>
-                  <Link
+                  {/* <Link
                     to={"menu/646b5548acd0a88a674b9429"}
                     className="px-2 py-1"
                   >
                     Menu
-                  </Link>
+                  </Link> */}
                   <Link to={"about"} className="px-2 py-1">
-                    About
+                    Sobre
                   </Link>
                   <Link to={"contact"} className="px-2 py-1">
-                    Contact
+                    Contacta
                   </Link>
                 </nav>
               </div>
